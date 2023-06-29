eb1980aa7229aaa6cdaf59d499cf686ce073cfc9
sandu <sandu@CRISTI>
2023-06-29 19:58:07 +0300

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program care va afisa ultima cifra a unui numar intreg citit de la tastaura*/
+
+            int i;
+            int lastDigit;
+
+            Console.WriteLine("Introduceti numarul:" + "\n");
+            i=int.Parse(Console.ReadLine());
+
+            lastDigit = i % 10;
+
+            Console.WriteLine("\n" + "Ultima cifra a numarului introdus este " + lastDigit + ".");
+
+        }
+    }
+}
